b1fa9e42dd2ad2cb926279ff75dad487163222b5
Manickam Manickam <59704334+manickamx2@users.noreply.github.com>
2023-01-22 12:41:11 -0800

    Add Day 14 part 1 solution

--- 14/day14.py
+++ 14/day14.py
@@ -1,18 +1,20 @@
+import math
+
 with open('day14-input.txt') as f:
     lines = f.readlines()
 
 
-def fill_grid(grid, y_max, x_max):
-    for y in range(y_max + 1):
-        for x in range(490, x_max + 1):
+def fill_grid(grid, y_max, x_max, x_min):
+    for y in range(y_max):
+        for x in range(x_min, x_max):
             if (x, y) not in grid:
                 grid[(x, y)] = Cell(x, y, 'air')
     return grid
 
 
-def print_grid(grid, y_max, x_max):
-    for y in range(y_max + 1):
-        for x in range(490, x_max + 1):
+def print_grid(grid, y_max, x_max, x_min):
+    for y in range(y_max):
+        for x in range(x_min, x_max):
             cell = grid.get((x, y))
             if cell:
                 print(str(cell), end='')
@@ -21,26 +23,9 @@ def print_grid(grid, y_max, x_max):
         print()
 
 
-def fill_gaps(prev):
-    prev_x, prev_y = prev
-    # horizontal gaps
-    if x > prev_x:
-        for i in range(prev_x + 1, x + 1):
-            grid[(i, y)] = Cell(i, y, 'rock')
-    elif prev_x > x:
-        for i in range(x + 1, prev_x + 1):
-            grid[(i, y)] = Cell(i, y, 'rock')
-    # vertical gaps
-    if y > prev_y:
-        for i in range(prev_y + 1, y + 1):
-            grid[(x, i)] = Cell(x, i, 'rock')
-    elif prev_y > y:
-        for i in range(x + 1, prev_x + 1):
-            grid[(x, i)] = Cell(x, i, 'rock')
-
-
-def calculate_next_position(gx, gy):
+def calculate_next_position(sand):
     # down, down-left, down-right
+    gx, gy = sand
     directions = [(0, 1), (-1, 1), (1, 1)]
     for dx, dy in directions:
         x = gx + dx
@@ -49,13 +34,13 @@ def calculate_next_position(gx, gy):
 
         if not cell:
             # we have reached the void
-            return x, y, False, True
-        if cell.type == 'air':
+            return None, None
+        elif cell.type != 'rock' and cell.type != 'sand':
             # we can reach this position, return
-            return x, y, False, False
+            return x, y
 
     # blocked in all directions
-    return gx, gy, True, False
+    return gx, gy
 
 
 class Cell:
@@ -77,51 +62,63 @@ class Cell:
 
 
 grid = {}
+x_min = math.inf
 x_max, y_max = 0, 0
 for line in lines:
     rocks = line.strip().split(' -> ')
-    prev = None
-    for r in rocks:
-        coordinate = r.split(',')
-        x, y = int(coordinate[0]), int(coordinate[1])
+    start = rocks[0].split(',')
+    start_x, start_y = int(start[0]), int(start[1])
+    for r in range(1, len(rocks)):
+        coordinates = rocks[r].split(',')
+        end_x, end_y = int(coordinates[0]), int(coordinates[1])
 
-        if prev:  # fill gaps
-            fill_gaps(prev)
-        x_max = max(x, x_max)
-        y_max = max(y, y_max)
-        cell = Cell(x, y, 'rock')
-        grid[(x, y)] = cell
-        prev = (x, y)
+        while start_x != end_x:
+            x_max = max(start_x, x_max)
+            x_min = min(start_x, x_min)
+            grid[(start_x, start_y)] = Cell(start_x, start_y, 'rock')
+            if start_x > end_x:
+                start_x -= 1
+            else:
+                start_x += 1
+
+        while start_y != end_y:
+            y_max = max(start_y, end_y, y_max)
+            grid[(start_x, start_y)] = Cell(start_x, start_y, 'rock')
+            if start_y > end_y:
+                start_y -= 1
+            else:
+                start_y += 1
+
+        x_max = max(start_x, end_x, x_max)
+        y_max = max(start_y, end_y, y_max)
+
+        grid[(end_x, end_y)] = Cell(end_x, end_y, 'rock')
+        start_x, start_y = end_x, end_y
 
 # sand source is (500, 0)
 source = (500, 0)
 grid[source] = Cell(500, 0, 'source')
-grid = fill_grid(grid, y_max, x_max)
-
-print_grid(grid, y_max, x_max)
-print("-"*100)
+grid = fill_grid(grid, y_max, x_max, x_min)
 
 # simulate sand falling
 sand_can_fall = True
 grains_of_sand = 0
 while sand_can_fall:
     # sand can fall, initialize a grain of sand
-    gx, gy = source
+    sand = source
     at_rest = False
     while not at_rest:
-        gx, gy, blocked, endless_void = calculate_next_position(gx, gy)
+        nx, ny = calculate_next_position(sand)
 
-        if endless_void:
-            print((gx, gy))
+        if (nx, ny) == (None, None):
             sand_can_fall = False
             at_rest = True
-
-        if blocked:
+        elif (nx, ny) == sand:
             # update the grid
-            grid[(gx, gy)].type = 'sand'
-            at_rest = True
+            grid[(nx, ny)].type = 'sand'
             grains_of_sand += 1
-    print_grid(grid, y_max, x_max)
-    print("-"*100)
+            at_rest = True
+
+        sand = (nx, ny)
 
 print(f"Part 1 answer: {grains_of_sand}")
